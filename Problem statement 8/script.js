308e05f44881d717f1215c3c92d73965c4ee247b
humble-guyy <agarwalpranjal2006@gmail.com>
2026-01-31 10:42:54 +0530

    statement 9 solved

---
+++ Problem statement 8/script.js
@@ -0,0 +1,10 @@
+//the output will be 
+// Hello
+// Error
+
+//explanation:
+//functions are hoisted so there will be problem and the sayHello() function will execute easily
+
+
+//var sayHi is hoisted but the function is not Hoisted so it will give error
+
